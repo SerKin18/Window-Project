4fd725a72e1edbdb56869545a1cd8a9757fb8843
Sergey <sergeykinyak@gmail.com>
2023-11-14 20:09:03 +0200

    (9 point of task) imgPopup

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,62 +1,61 @@
 import checkNumInputs from "./checkNumInputs";
 
 const forms = (state) => {
-	const form = document.querySelectorAll('form'),
-		inputs = document.querySelectorAll('input');
-		// phoneInputs = document.querySelectorAll('input[name="user_phone"]');
+  const form = document.querySelectorAll("form"),
+    inputs = document.querySelectorAll("input");
+  // phoneInputs = document.querySelectorAll('input[name="user_phone"]');
 
-checkNumInputs('input[name="user_phone"]')
+  checkNumInputs('input[name="user_phone"]');
 
-	const message = {
-		loading: 'Загрузка...',
-		success: 'Спасибо! Скоро с вами свяжемся...',
-		failure: 'Что-то пошло не так..'
-	};
+  const message = {
+    loading: "Загрузка...",
+    success: "Спасибо! Скоро с вами свяжемся...",
+    failure: "Что-то пошло не так..",
+  };
 
-	const postDATA = async (url, data) => {
-		document.querySelector('.status').textContent = message.loading;
-		let res = await fetch(url, {
-			method: "POST",
-			body: data
-		});
-		return await res.text();
+  const postDATA = async (url, data) => {
+    document.querySelector(".status").textContent = message.loading;
+    let res = await fetch(url, {
+      method: "POST",
+      body: data,
+    });
+    return await res.text();
+  };
 
-	};
+  const clearInputs = () => {
+    inputs.forEach((item) => {
+      item.value = "";
+    });
+  };
 
-	const clearInputs = () => {
-		inputs.forEach(item => {
-			item.value = '';
-		})
-	}
+  form.forEach((item) => {
+    item.addEventListener("submit", (e) => {
+      e.preventDefault();
 
-	form.forEach(item => {
-		item.addEventListener('submit', (e) => {
-			e.preventDefault();
+      let statusMessage = document.createElement("div");
+      statusMessage.classList.add("status");
+      item.appendChild(statusMessage);
 
-			let statusMessage = document.createElement('div');
-			statusMessage.classList.add('status');
-			item.appendChild(statusMessage);
+      const formData = new FormData(item);
+      if (item.getAttribute("data-calc") === "end") {
+        for (let key in state) {
+          formData.append(key, state[key]);
+        }
+      }
 
-			const formData = new FormData(item)
-			if(item.getAttribute('data-calc')==='end'){
-				for(let key in state){
-					formData.append(key,state[key])
-				}
-			}
-
-			postDATA('../../assets/server.php', formData)
-				.then(res => {
-					console.log(res);
-					statusMessage.textContent = message.success
-				})
-				.catch(() => statusMessage.textContent = message.failure)
-				.finally(() => {
-					clearInputs();
-					setTimeout(() => {
-						statusMessage.remove()
-					}, 5000);
-				})
-		})
-	})
-}
-export default forms
+      postDATA("../../assets/server.php", formData)
+        .then((res) => {
+          console.log(res);
+          statusMessage.textContent = message.success;
+        })
+        .catch(() => (statusMessage.textContent = message.failure))
+        .finally(() => {
+          clearInputs();
+          setTimeout(() => {
+            statusMessage.remove();
+          }, 5000);
+        });
+    });
+  });
+};
+export default forms;
